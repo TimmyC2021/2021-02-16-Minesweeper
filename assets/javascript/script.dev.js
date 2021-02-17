0dbab58b082e06d999dede2573a64b37ba8221c0
Tim Carr <github@timcarr.co.uk>
2021-02-17 01:53:46 +0000

    Update 3 Game now generates own grid according to size selected

--- assets/javascript/script.dev.js
+++ assets/javascript/script.dev.js
@@ -6,23 +6,86 @@
 // right click - flag it (or unflag it)
 // left+right - test it and adjoining squares
 // Routine to generate different sized grids
-// Need to know size of grid Rows x Columns
-// Final game to offer 3 preset options and maybe a custom
-// For testing purposes lets work with 5 x 5
-var rowsX = 5;
-var columnsY = 5; // Populate grid with mines
+// Final game to offer 3 preset options and maybe a custom option
+// How do we read the size?
+var gameSize = function gameSize(event) {
+  // Not sure if this routine is needed???
+  // Maybe if we want to change some formatting between selection and execution
+  // DEBUG message                                                        ================================
+  console.log("gameSize ".concat(event.target.title)); // End of DEBUG message                                                 ================================
+  // selected option to be highlighted in yellow until actioned (in Let's Play)
+  // document.getElementById(event.target.id).classList.add('highlightChange') ;
+}; // other options to be cleared of highlight
+// Difficulty level
+
+
+var difficulty = function difficulty(event) {
+  // DEBUG message                                                        ================================
+  console.log("difficulty ".concat(event.target.value)); // End of DEBUG message                                                 ================================
+}; // Function to create new grid
+// It will generate the neccessary html code to insert into the game-area elements
+
+
+var createGrid = function createGrid(rowsX, columnsY) {
+  var gridInnerHtml = "";
+  var gridColumns = ""; // Create the string for the inner HTML
+
+  for (var rowIndex = 1; rowIndex <= rowsX; rowIndex++) {
+    for (var columnIndex = 1; columnIndex <= columnsY; columnIndex++) {
+      var rowsString = rowIndex.toString().padStart(2, "0");
+      var columnString = columnIndex.toString().padStart(2, "0");
+      gridInnerHtml = " ".concat(gridInnerHtml, " <div class=\"game-area__mine\" id=\"mine").concat(rowsString).concat(columnString, "\" onclick=\"mineField(event)\"></div>");
+    }
+  } // Generate a string to set the correct column spacings in the grid-box for game-area
+
+
+  for (var index = 0; index < columnsY; index++) {
+    gridColumns = gridColumns + " 1fr";
+  } // Set both the column styling for the grid and the HTML for the cells
+
+
+  var gameArea = document.getElementById('game-area');
+  gameArea.style.gridTemplateColumns = gridColumns;
+  gameArea.innerHTML = gridInnerHtml;
+}; // Populate grid with mines
 // Game to offer 3 levels of difficulty. Easy, Medium, Hard
 // These reflect a density of mines, so total number may change with size of grid for the same level of difficulty
 // Calculate nearby mines for every cell without a mine
 // Need to go around the surrounding 8 (max) cells looking for mines (value=9)
-// How to systamatically work outwards from selected cell??
+// How to systematically work outwards from selected cell??
 // This routine may need to be called from different sections
 // Perhaps use an array of cells to be checked
-// Grid referencing, will be slightly at odds with array indices
-// Rows 10 to xx (xx == X rows + 10) starting at 10 to force double digits
-// Columns 01 to yy (yy == Y columns + 10) starting at 10 to force double digits
-// Let's play big Yellow button
+// Grid referencing
+// Mine0205 refers to the second row and 5th column
+// Function to return which size option has been selected
+
+
+var getSize = function getSize() {
+  // reads the title of the relevant radio button
+  // it expects it to be in a format "row x column" with spaces around the x
+  //const gridSize = document.querySelector("input[name=gameSize]:checked").title.split(' x ');
+  return document.querySelector("input[name=gameSize]:checked").title.split(' x ');
+}; // Let's play  ===  big Yellow button
+
 
 function resetPlay(event) {
-  console.log("We are in resetPlay");
-}
+  // DEBUG message                                                        ================================
+  console.log("We are in resetPlay and getSize is: ".concat(getSize())); // End of DEBUG message                                                 ================================
+  // Check the size of grid required
+
+  var gridSize = getSize(); // Create the grid using size from above
+
+  createGrid(gridSize[0], gridSize[1]); // Generate mines in grid
+  // Calculate nearby mines for each cell not containing a mine
+} // This function is called when a cell in the grid is clicked on
+
+
+function mineField(event) {
+  // DEBUG message                                                        ================================
+  console.log("mineField ".concat(event.target.id)); // End of DEBUG message                                                 ================================
+} // DEBUG message                                                        ================================
+// End of DEBUG message                                                 ================================
+// Start the game
+
+
+resetPlay();
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,14 +6,60 @@
 
 
 // Routine to generate different sized grids
-  // Need to know size of grid Rows x Columns
-  // Final game to offer 3 preset options and maybe a custom
-  // For testing purposes lets work with 5 x 5
 
-  const rowsX = 5;
-  const columnsY = 5;
+// Final game to offer 3 preset options and maybe a custom option
+    // How do we read the size?
+    const gameSize = (event) => {
+      // Not sure if this routine is needed???
+      // Maybe if we want to change some formatting between selection and execution
+
+      // DEBUG message                                                        ================================
+      console.log(`gameSize ${event.target.title}`);
+      // End of DEBUG message                                                 ================================
+    
+      // selected option to be highlighted in yellow until actioned (in Let's Play)
+      // document.getElementById(event.target.id).classList.add('highlightChange') ;
+    }
+    // other options to be cleared of highlight
 
 
+
+// Difficulty level
+const difficulty = (event) => {
+
+      // DEBUG message                                                        ================================
+      console.log(`difficulty ${event.target.value}`);
+      // End of DEBUG message                                                 ================================
+
+}
+
+
+
+  // Function to create new grid
+    // It will generate the neccessary html code to insert into the game-area elements
+    const createGrid = (rowsX,columnsY) => {
+      let gridInnerHtml = "";
+      let gridColumns = "";
+  
+      // Create the string for the inner HTML
+      for (let rowIndex=1; rowIndex <= rowsX; rowIndex++ ) {
+        for (let columnIndex = 1; columnIndex <= columnsY; columnIndex++) {
+          let rowsString = rowIndex.toString().padStart(2,"0");
+          let columnString = columnIndex.toString().padStart(2,"0");
+          gridInnerHtml = ` ${gridInnerHtml} <div class="game-area__mine" id="mine${rowsString}${columnString}" onclick="mineField(event)"></div>`
+        }
+      }
+
+      // Generate a string to set the correct column spacings in the grid-box for game-area
+      for (let index=0; index < columnsY; index++) {
+        gridColumns = gridColumns + " 1fr"
+      }
+
+      // Set both the column styling for the grid and the HTML for the cells
+      let gameArea = document.getElementById('game-area');
+      gameArea.style.gridTemplateColumns = gridColumns;
+      gameArea.innerHTML = gridInnerHtml;
+    }
 
 // Populate grid with mines
   // Game to offer 3 levels of difficulty. Easy, Medium, Hard
@@ -22,16 +68,50 @@
 // Calculate nearby mines for every cell without a mine
   // Need to go around the surrounding 8 (max) cells looking for mines (value=9)
 
-// How to systamatically work outwards from selected cell??
+// How to systematically work outwards from selected cell??
   // This routine may need to be called from different sections
   // Perhaps use an array of cells to be checked
 
-// Grid referencing, will be slightly at odds with array indices
-  // Rows 10 to xx (xx == X rows + 10) starting at 10 to force double digits
-  // Columns 01 to yy (yy == Y columns + 10) starting at 10 to force double digits
+// Grid referencing
+  // Mine0205 refers to the second row and 5th column
 
-
-  // Let's play big Yellow button
-  function resetPlay(event) {
-    console.log("We are in resetPlay");
+  // Function to return which size option has been selected
+  const getSize = () => {
+    // reads the title of the relevant radio button
+    // it expects it to be in a format "row x column" with spaces around the x
+    //const gridSize = document.querySelector("input[name=gameSize]:checked").title.split(' x ');
+    return document.querySelector("input[name=gameSize]:checked").title.split(' x ');
   }
+
+  // Let's play  ===  big Yellow button
+
+  function resetPlay(event) {
+    // DEBUG message                                                        ================================
+    console.log(`We are in resetPlay and getSize is: ${getSize()}`);
+    // End of DEBUG message                                                 ================================
+    
+    // Check the size of grid required
+    const gridSize = getSize();
+
+    // Create the grid using size from above
+    createGrid(gridSize[0],gridSize[1]);
+
+    // Generate mines in grid
+
+    // Calculate nearby mines for each cell not containing a mine
+  }
+
+    // This function is called when a cell in the grid is clicked on
+    function mineField(event) {
+
+      // DEBUG message                                                        ================================
+      console.log(`mineField ${event.target.id}`);
+      // End of DEBUG message                                                 ================================
+    }
+
+      // DEBUG message                                                        ================================
+      // End of DEBUG message                                                 ================================
+
+
+      // Start the game
+      resetPlay();
